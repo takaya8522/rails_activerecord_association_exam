8bfa0db67563754b28d6cf0bc1a99f0b26fbd45c
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-04 17:35:03 +0900

    修正5

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,13 +15,13 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:orders).group('addresses.id').order('count(addresses.id) DESC').first
+    @address = Address.joins(:orders).select("addresses.*, COUNT(addresses.id) AS orders_count").group('addresses.id').order('orders_count DESC').first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.joins(orders: :foods).group('customers.id').order('sum(price) DESC').first
+    @customer = Customer.joins(orders: :foods).select("customers.*, sum(price) AS foods_price_sum").group('customers.id').order('foods_price_sum DESC').first
   end
 end
